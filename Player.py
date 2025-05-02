5ddd8664575cb6bf5c5d91374d9087c27cd5934e
Valtor91 <128702999+Valtor91@users.noreply.github.com>
2025-05-02 18:58:04 +0200

    jumps

--- Player.py
+++ Player.py
@@ -13,7 +13,7 @@ class Player(Entitie):
         self.is_jumping = False
         self.jump_force = -20
         self.gravity_force = 9.8
-
+        self.velocity_y = 0
         self.screen = screen
         self.VectorGravity = (0,1)
 
@@ -26,15 +26,24 @@ class Player(Entitie):
 
         if keys[pygame.K_q]:
             self.Position[0] -= self.MaxSpeed
-        pygame.draw.rect(self.screen, "red", (self.Position[0],self.Position[1], 10, 10))
-        print(self.Position)
-    def gravity(self):
-        if not self.Position[1] >= 660:
-            gravity = [self.Position[0], self.VectorGravity[1]*self.gravity_force + self.Position[1]]
 
-            self.Position[1] = gravity[1]
+        if keys[pygame.K_SPACE] and not self.is_jumping:
+            self.is_jumping = True
+            self.velocity_y = self.jump_force
+
+        pygame.draw.rect(self.screen, "red", (self.Position[0],self.Position[1], 10, 10))
+
+    def gravity(self):
+        gravity = [self.Position[0], self.VectorGravity[1] * self.velocity_y + self.Position[1]]
+
+        self.Position[1] = gravity[1]
+        if not self.Position[1] >= 660:
+            self.velocity_y += self.gravity_force*0.051
+
+
         else:
             self.Position[1] = 660
+            self.is_jumping = False
 
 
 
