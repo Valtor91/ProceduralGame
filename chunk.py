f3cdb6a068d4f61143232536f639188ebdaf1023
Valtor91 <128702999+Valtor91@users.noreply.github.com>
2025-05-03 18:35:48 +0200

    re bruit de perlin

--- chunk.py
+++ chunk.py
@@ -22,18 +22,46 @@ class Chunck:
         self.ground_level = screen.get_height() /2
         self.avencer = 0
         self.color = (255,255,255)
+        self.Value2D = 0
     def PerlinNoise(self):
+
 
         noise_surface = pygame.Surface((self.screen.get_width(), self.screen.get_height()))
 
         for x in range(0,self.screen.get_width(),10):
             Value1D = int(self.ground_level + noise.pnoise1(x / self.Scale, octaves=self.Octave, base=self.seed) * 100)
-            print(Value1D)
+
             for y in range(0,self.screen.get_height(),10):
                 if y < Value1D:
                     self.color = (135, 206, 250)
                 elif y == Value1D:
+                    self.color = self.Grass
+                elif y >= Value1D + 20:
+                    self.color = self.Rock
+
+
+                    if y >= Value1D+40:
+
+                        self.Value2D = noise.pnoise2(
+                            x / self.Scale,
+                            y / self.Scale,
+                            octaves=self.Octave,
+                            persistence=self.Persistence,
+                            lacunarity=self.Lacunarity,
+                            repeatx=self.screen.get_width(),
+                            repeaty=self.screen.get_height(),
+                            base=self.seed
+                        )
+                        if self.Value2D >= 0.2:
+                            self.color = (109, 109, 109  )
+                        else:
+                            self.color = (71, 71, 71  )
+
+                elif y > Value1D:
                     self.color = self.Dirt
+
+                    print(self.Value2D)
+
                 pygame.draw.rect(noise_surface, self.color, pygame.Rect(x, y, 10, 10))
 
         return noise_surface
